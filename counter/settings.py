da540f8e158ba06712dd1e0aa67ecb628ef6dfc3
PratyakshMathur <pratyaksh.mathur28@gmail.com>
2021-12-20 23:20:30 +0530

    Forgot password added

--- counter/settings.py
+++ counter/settings.py
@@ -132,3 +132,11 @@ STATICFILES_DIRS =[
 MEDIA_ROOT  = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
+
+#SMTP response
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'capstonejenkinsmailing@gmail.com'
+EMAIL_HOST_PASSWORD = 'sussyboys@123'
--- humancounter/urls.py
+++ humancounter/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -15,4 +16,21 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="humancounter/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="humancounter/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name="humancounter/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="humancounter/password_reset_done.html"), 
+        name="password_reset_complete"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
